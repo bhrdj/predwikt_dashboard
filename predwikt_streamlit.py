61c61e6a50169e53ed611e260c92de61069d3dd7
bhrdj <steven@bhrdwj.net>
2022-03-11 10:57:42 +0800

    progress toward arima

--- predwikt_streamlit.py
+++ predwikt_streamlit.py
@@ -10,10 +10,14 @@ from sklearn.model_selection import GridSearchCV
 
 st.title('Predwikt')
 
-datapath = '../data/'
+datapath = './data/'
 with open(datapath + 'disaster_daily_edits.pickle', 'rb') as f:
     disaster_daily_edits = pickle.load(f)
 
-st.
+train_weeks = st.slider('min_train', 
+                        min_value=0, 
+                        max_value=disaster_daily_edits.shape[0]-200)
+start_date = st.slider('max_train', min_value=min_train+100, max_value=disaster_daily_edits.shape[0]-2)
+
 
 st.write('hiiiii')
